ee1fca49bfec58296952a9ec60cfd9d5b4488048
cbustamanter <cesar.raggio1@gmail.com>
2021-03-13 12:59:35 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="hero is-link is-fullheight">
+ <!--  <div class="hero-body">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  </div> -->
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <p class="title">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+      </p>
+    </div>
+  </div>
+</section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import './assets/styles/scss/global.scss'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
