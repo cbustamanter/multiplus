203046f9dd116393738c2fc2af644e4f59ff00e1
cbustamanter <cesar.raggio1@gmail.com>
2021-04-22 00:04:35 -0500

    clients

--- src/components/Clients.vue
+++ src/components/Clients.vue
@@ -4,6 +4,20 @@
       <h1 class="title bold-text">Nuestros Clientes</h1>
     </div>
   </div>
+  <div class="columns is-mobile clients-container is-multiline">
+    <div class="column is-3"><img src="../assets/images/logos/mallplaza.png" alt="mall"></div>
+    <div class="column is-3"><img src="../assets/images/logos/oechsle.png" alt="mall"></div>
+    <div class="column is-3"><img src="../assets/images/logos/bosch.png" alt="mall"></div>
+    <div class="column is-3"><img src="../assets/images/logos/claro.png" alt="mall"></div>
+    <div class="column is-3"><img src="../assets/images/logos/lapositiva.png" alt="mall"></div>
+    <div class="column is-3"><img src="../assets/images/logos/viva.png" alt="mall"></div>
+    <div class="column is-3"><img src="../assets/images/logos/cajatrujillo.png" alt="mall"></div>
+    <div class="column is-3"><img src="../assets/images/logos/carsa.png" alt="mall"></div>
+    <div class="column is-3"><img src="../assets/images/logos/volkswagen.png" alt="mall"></div>
+    <div class="column is-3"><img src="../assets/images/logos/barrick.png" alt="mall"></div>
+    <div class="column is-3"><img src="../assets/images/logos/directv.png" alt="mall"></div>
+    <div class="column is-3"><img src="../assets/images/logos/interbank.png" alt="mall"></div>
+  </div>
 
 </template>
 
@@ -21,6 +35,12 @@ h1.title {
   @include until($tablet) {
     font-size: 30px;
     text-align: center;
+  }
+}
+.clients-container {
+  padding: 3vh 12vw;
+  .column {
+    padding-bottom: 2rem;
   }
 }
 </style>
